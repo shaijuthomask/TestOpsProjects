9c296711158f10aa47b674b9b6b1bbf5b65ce6a2
shaijuthomask@gmail.com <shaijuthomask@gmail.com>
2020-08-08 23:27:37 +1000

    Syst Testing

---
+++ InspireTest/src/test/SystemTesting.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Test;
+
+public class SystemTesting {
+	  @Test
+	   private void testSystemTest() {
+	      String str= "System test passed on 8 Aug";
+	    System.out.println(str);
+	}
+}
